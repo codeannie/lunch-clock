a0caa0fe27ac67a1a160393d9a2708f158870779
Annie Chiang <devbear22@gmail.com>
2018-07-12 21:12:44 -0700

    add current date & date

--- app.js
+++ app.js
@@ -2,11 +2,20 @@
 
 const format = require('date-fns/format');
 
+// DATE
+function showDate() {
+  const todayDate = format(new Date(), 'ddd, MMM DD, YYYY');
+
+  $('#date').html(todayDate);
+}
+
 // CLOCK
 function showTime() {
   const currentTime = format(new Date(), 'hh:mm:ss A');
 
   $('#clock').html(currentTime);
 }
+
+showDate();
 setInterval(showTime, 1000);
 
