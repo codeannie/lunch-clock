e2957d40679ef99d1031bf2b23e460eb5fc7bc0a
Annie Chiang <devbear22@gmail.com>
2018-07-12 21:11:36 -0700

    streamlined clock code with date-fns

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
 'use strict';
 
-// DIGITAL CLOCK
+const format = require('date-fns/format');
 
+// CLOCK
 function showTime() {
-  const time = new Date();
-  const hour = time.getHours();
-  const minutes = time.getMinutes();
-  const seconds = time.getSeconds();
-  let  am_pm = '';
+  const currentTime = format(new Date(), 'hh:mm:ss A');
 
-  let currentTime = hour + ':' + minutes + ':' + seconds;
-
-  // if (hr >= 12) {
-  //   hr -= 12;
-  //   am_pm = 'PM';
-  // } else if ( hr ===  0 ) {
-  //   hr = 12;
-  //   am_pm = 'AM';
-  // }
-  // return currentTime;
   $('#clock').html(currentTime);
-};
-
-console.log(showTime());
-
+}
 setInterval(showTime, 1000);
 
