9245a564992d1c95ab31c2886f0c59d42f0faac4
Annie Chiang <devbear22@gmail.com>
2018-07-14 21:55:20 -0700

    select component only captures initial value -- still testing

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 'use strict';
 const format = require('date-fns/format');
 
+const STATE = {
+  breakLength: '',
+  timeOut: '',
+  timeIn:'',
+};
+
 // DATE
 function showDate() {
   const todayDate = format(new Date(), 'ddd, MMM DD, YYYY');
@@ -21,5 +27,17 @@ setInterval(showTime, 1000);
 // TIME PICKER
 const select = new mdc.select.MDCSelect(document.querySelector('.mdc-select'));
 select.listen('change', () => {
-  alert(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
+  console.log(`Selected option at index ${select.selectedIndex} with value "${select.value}"`);
 });
+
+// convert time out input w date fns
+/* function getOutTime() {
+  const hourOut = $('.hr').value;
+  const minOut = $('.min').value;
+  const amOrPm = $('.am_pm').value;
+
+  const timeOut = hourOut + ':' + minOut + amOrPm;
+  console.log('out time', timeOut);
+} */
+
+// add break length to time out time 
