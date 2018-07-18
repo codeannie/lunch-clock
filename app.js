94578e4c798b42352c5124157610aecb8522c150
Annie Chiang <devbear22@gmail.com>
2018-07-18 16:56:33 -0700

    fixed - flatpickr working; was missing CSS file import to display correctly

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
-'use strict';
-import { MDCSelect } from '@material/select';
-import { MDCFloatingLabel } from '@material/floating-label';
+// import { MDCSelect } from '@material/select';
+// import { MDCFloatingLabel } from '@material/floating-label';
 import { MDCRipple } from '@material/ripple';
 import flatpickr from 'flatpickr';
 import { addMinutes, format, parse, setHours, setMinutes, min } from 'date-fns';
 import { getTodaysDate, getCurrentTime } from './client/current-dateTime';
-
 
 const STATE = {
   breakLength: '',
@@ -13,32 +11,47 @@ const STATE = {
   minute: '',
   period: '',
   timeOut: '',
-  timeIn:'',
+  timeIn: '',
 };
 
 // DISPLAY CURRENT DATE & TIME
 getTodaysDate();
 setInterval(getCurrentTime, 1000);
 
+// populate input with current time to save clicks
 // FLATPICKR (Time Picker library)
-$(".flatpickr").flatpickr({
-  enableTime: true,
-  noCalendar: true,
-  dateFormat: "H:i",
-  time_24hr: true
-});
+const timePicker = () => {
+  flatpickr('#flatpickr', {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: 'H:i',
+    time_24hr: true,
+    minuteIncrement: 1,
+    defaultHour: format(new Date(), 'hh'),
+    defaultMinute: format(new Date(), 'mm'),
+  });
+};
 
-// INSTANTIATE FOR MATERIAL COMPONENTS 
+timePicker();
+
+// $(".flatpickr").flatpickr({
+//   enableTime: true,
+//   noCalendar: true,
+//   dateFormat: "H:i",
+//   time_24hr: true
+// });
+
+// INSTANTIATE FOR MATERIAL COMPONENTS
 // BUTTON RIPPLE EFFECT
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 
 // FLOATING LABEL EFFECT
-const floatingLabel = new MDCFloatingLabel(document.querySelector('.mdc-floating-label'));
+// const floatingLabel = new MDCFloatingLabel(document.querySelector('.mdc-floating-label'));
 
 // SELECT HOUR
-const selectHour = new MDCSelect(document.querySelector('.select-hour'));
+/* const selectHour = new MDCSelect(document.querySelector('.select-hour'));
 selectHour.listen('change', () => {
-  STATE.hour = setHours(new Date(),`${selectHour.value}`);
+  STATE.hour = setHours(new Date(), `${selectHour.value}`);
   console.log('STATE - hour:', STATE.hour);
   console.log(`Selected option at index ${selectHour.selectedIndex} with value "${selectHour.value}"`);
 });
@@ -58,15 +71,16 @@ selectAmPm.listen('change', () => {
   console.log('STATE - period:', STATE.period);
   console.log(`Selected option at index ${selectAmPm.selectedIndex} with value "${selectAmPm.value}"`);
 });
+*/
 
 // GET LUNCH OUT TIME
-function getClockOutTime(){
-  const timeOut = STATE.hour + ':' + STATE.minute;
+function getClockOutTime() {
+  const timeOut = `${STATE.hour}:${STATE.minute}`;
   STATE.timeOut = format(timeOut);
   console.log('STATE - time out? ->', STATE.timeOut);
 }
 
-// 30 MINUTES BREAK 
+// 30 MINUTES BREAK
 $('.mdc-button').on('click', () => {
   // getClockOutTime();
   // alert('break button clicked');
@@ -74,7 +88,7 @@ $('.mdc-button').on('click', () => {
   STATE.breakLength = $(this).value;
   console.log('break length button ->', `${this}`);
   console.log('break length? ->', STATE.breakLength);
-})
+});
 
 // convert time out input w date fns
 // function getOutTime() {
@@ -86,4 +100,4 @@ $('.mdc-button').on('click', () => {
 // };
 // getOutTime();
 
-// add break length to time out time 
+// add break length to time out time
