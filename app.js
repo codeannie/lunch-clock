3ddb039f50174bbb20f853a0d41f5defbd8b82ee
Annie Chiang <devbear22@gmail.com>
2018-07-19 10:39:10 -0700

    successful material design textfield usage & calculate time by button selected

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-// import { MDCSelect } from '@material/select';
+import { MDCTextField } from '@material/textfield';
 // import { MDCFloatingLabel } from '@material/floating-label';
 import { MDCRipple } from '@material/ripple';
 import flatpickr from 'flatpickr';
-import { addMinutes, format, parse, setHours, setMinutes, min } from 'date-fns';
+import { addMinutes, format } from 'date-fns';
 import { getTodaysDate, getCurrentTime } from './client/current-dateTime';
 
 const STATE = {
@@ -22,44 +22,28 @@ const timePicker = flatpickr('#flatpickr', {
   enableTime: true,
   noCalendar: true,
   dateFormat: 'H:i',
-  time_24hr: true,
+  // time_24hr: true,
   minuteIncrement: 1,
   defaultHour: format(new Date(), 'HH'),
   defaultMinute: format(new Date(), 'mm'),
 });
 
-// const enteredTime = $('#flatpickr').attr('data-id').value;
-// const enteredTime = $('#flatpickr').val();
-// console.log('flatpickr value?', timePicker.value());
-
 // INSTANTIATE FOR MATERIAL COMPONENTS
+// TEXT FIELD
+const textField = new MDCTextField(document.querySelector('.mdc-text-field'));
+
 // BUTTON RIPPLE EFFECT
 const buttonRipple = new MDCRipple(document.querySelector('.mdc-button'));
 
 // FLOATING LABEL EFFECT
 // const floatingLabel = new MDCFloatingLabel(document.querySelector('.mdc-floating-label'));
 
-// GET LUNCH OUT TIME
-// function getClockOutTime() {
-//   const timeOut = `${STATE.hour}:${STATE.minute}`;
-//   STATE.timeOut = format(timeOut);
-//   console.log('STATE - time out? ->', STATE.timeOut);
-// }
-
 // 30 MINUTES BREAK
-$('.mdc-button').on('click', () => {
-  // getClockOutTime();
-  alert('clicked!');
+$('.mdc-button').on('click', e => {
+  // alert('clicked!');
+  const breakLength = $(e.currentTarget).data('min');
   console.log('date obj', new Date());
-  console.log('timepicker value', timePicker.selectedDates[0]);
-  console.log('timepicker value + 30', addMinutes(timePicker.selectedDates[0], 30));
-  // console.log('flatpickr value?', timePicker.value());
-  // STATE.breakLength = $(this).attr('data-min');
-  STATE.breakLength = $(this).value;
-  // console.log('break length button ->', `${this}`);
-  // console.log('break length? ->', STATE.breakLength);
+  console.log('button data min->', breakLength);
+  console.log('time out', timePicker.selectedDates[0]);
+  console.log('time out + mins', addMinutes(timePicker.selectedDates[0], breakLength));
 });
-
-// convert time out input w date fns
-
-// add break length to time out time
